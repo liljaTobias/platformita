e34cc65604f6606849799f54fce62382357a2107
Tobias Lilja <lilja.tobias94@gmail.com>
2023-07-27 16:44:35 +0200

    feat: initial game

---
+++ Assets/PlayerCamera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 0, -10);
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
---
+++ Assets/playerscript.cs
@@ -0,0 +1,208 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerscript : MonoBehaviour
+{
+    [SerializeField] public Rigidbody2D rb;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+
+    private float horizontal;
+
+    public float speed;
+    public float jumpHeight = 5;
+    public float buttonTime = 0.3f;
+    public float cancelRate = 100;
+
+    [Header("Ground collision")]
+    public Vector2 boxSize;
+    public float castDistance;
+    public LayerMask groundLayer;
+
+    [Header("Wall jumping")]
+    public float wallSlidingSpeed = 2f;
+    private bool isWallSliding;
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    public float wallJumpingDuration = 0.4f;
+    public Vector2 wallJumpingPower;
+
+    bool jumping;
+    bool jumpCancelled;
+    float jumptime;
+
+    private Animator anim;
+
+    private bool isFacingRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isFacingRight = true;
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        HandleJump();
+
+        WallSlide();
+        WallJump();
+
+        if (!isWallJumping)
+        {
+            Flip();
+        }
+
+        HandleAnimations();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isWallJumping)
+        {
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        }
+
+        if (jumpCancelled && jumping && rb.velocity.y > 0)
+        {
+            rb.AddForce(Vector2.down * cancelRate);
+        }
+    }
+
+    public bool isGrounded()
+    {
+        if(Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
+        {
+           return true;
+        }
+
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position-transform.up * castDistance, boxSize);
+    }
+
+    public void Flip()
+    {
+        if (!isFacingRight && horizontal > 0f || isFacingRight && horizontal < 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded()){
+            float jumpForce = Mathf.Sqrt(jumpHeight * -2 * (Physics2D.gravity.y * rb.gravityScale));
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            jumping = true;
+            jumptime = 0;
+            jumpCancelled = false;
+        }
+
+        if (jumping)
+        {            
+            jumptime += Time.deltaTime;
+            if (Input.GetKeyUp(KeyCode.Space) | jumptime > buttonTime)
+            {
+                jumpCancelled = true;
+            }
+            if(jumptime > buttonTime)
+            {
+                jumping = false;
+            }
+        }
+
+    }
+
+    private bool isWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    private void WallSlide()
+    {
+        if(isWalled() && !isGrounded() && horizontal != 0f)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    private void WallJump()
+    {
+        if(isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+            rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if(transform.localScale.x != wallJumpingDirection)
+            {
+                isFacingRight = !isFacingRight;
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
+
+    private void HandleAnimations()
+    {
+        if (horizontal != 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        if (rb.velocity.y < 0 && !isGrounded())
+        {
+            anim.SetBool("isFalling", true);
+        }
+        else
+        {
+            anim.SetBool("isFalling", false);
+        }
+
+        anim.SetBool("isJumping", !isGrounded());
+    }
+}
